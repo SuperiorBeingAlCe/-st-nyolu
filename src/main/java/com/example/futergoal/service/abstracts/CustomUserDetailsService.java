921d258128ca87344c5b5d5743989bedb865f06d
Alperen Çelebi <160335144+SuperiorBeingAlCe@users.noreply.github.com>
2025-05-01 12:54:35 +0300

    Delete src/main/java/com/example/futergoal/service/abstracts/CustomUserDetailsService.java

--- src/main/java/com/example/futergoal/service/abstracts/CustomUserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.futergoal.service.abstracts;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface CustomUserDetailsService {
-	
-	public UserDetails loadUserByUsername(String username);
-}
