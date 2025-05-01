b890dce53dafe38f50dc59fc98108d45e1805322
Alperen Çelebi <160335144+SuperiorBeingAlCe@users.noreply.github.com>
2025-05-01 16:33:00 +0300

    Update User.java

--- src/main/java/com/example/futergoal/entities/User.java
+++ src/main/java/com/example/futergoal/entities/User.java
@@ -1,9 +1,7 @@
 package com.example.futergoal.entities;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -16,8 +14,6 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
