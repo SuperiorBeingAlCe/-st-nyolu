76478a2e8cfd43fb18a1b200aa42e062d1958538
Alperen Çelebi <160335144+SuperiorBeingAlCe@users.noreply.github.com>
2025-05-01 16:38:59 +0300

    Update DtoUserIU.java

--- src/main/java/com/example/futergoal/dto/DtoUserIU.java
+++ src/main/java/com/example/futergoal/dto/DtoUserIU.java
@@ -9,9 +9,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Data
 @NoArgsConstructor
